00412e022f61653bbab0e690bfe6b899de5efa71
stefancross <stefan.cross@o2.com>
2014-10-31 23:42:09 +0000

    Starting planner module

---
+++ src/planner.erl
@@ -0,0 +1,23 @@
+%%%-------------------------------------------------------------------
+%%% @author stefancross
+%%% @copyright (C) 2014, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 31. Oct 2014 23:18
+%%%
+%%% 
+%%% The following API should be respected when implementing the server:
+%%%
+%%% planner:start_link()  -> {ok, Pid}.
+%%%
+%%% Start a new process that is linked to the calling process.
+%%%
+%%% planner:route(From, ToList) -> {ok, CityList} | {error, invalid}.
+%%%
+%%%-------------------------------------------------------------------
+-module(planner).
+-author("stefancross").
+
+%% API
+-export([]).
