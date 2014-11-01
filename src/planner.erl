5cbcdff690d7788fcebc3dde4f26939c75278b8e
stefancross <stefan.cross@o2.com>
2014-11-01 00:38:27 +0000

    baisics

--- src/planner.erl
+++ src/planner.erl
@@ -6,7 +6,7 @@
 %%% @end
 %%% Created : 31. Oct 2014 23:18
 %%%
-%%% 
+%%%
 %%% The following API should be respected when implementing the server:
 %%%
 %%% planner:start_link()  -> {ok, Pid}.
@@ -20,4 +20,19 @@
 -author("stefancross").
 
 %% API
--export([]).
+-export([start_link/0, route/2]).
+
+start_link() ->
+  register(planner, spawn_link(?MODULE, loop, [])),
+  {ok, planner}.
+
+route(from, toList) ->
+  io:format("magic_happens_here... ~p~n", [{from, toList}]),
+  loop().
+
+loop() ->
+  receive
+    {from, list} -> route(from, list);
+    stop -> exit(stopped)
+  end,
+loop().
