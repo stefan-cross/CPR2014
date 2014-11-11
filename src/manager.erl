1f75e007846b5abd01cf22053f97f2694608f4f9
stefancross <stefan.cross@o2.com>
2014-11-11 18:52:03 +0000

    Working on send/3 formatting with unique ref and also deliver/1 with ets support

--- src/manager.erl
+++ src/manager.erl
@@ -13,19 +13,28 @@
 -export([start_link/0, deliver/1, loop/0, send/3]).
 
 start_link() ->
-  register(?MODULE, spawn_link(?MODULE, loop, [])).
+  register(?MODULE, spawn_link(?MODULE, loop, [])),
+  ets:new(manager, [set, named_table]).
+
+
+uniqueref({A, B, C}) ->
+  ((A * 1000) * 4) + ((B * 1000) * 2) + C.
 
 send(From, To, Kg) ->
-  manager ! {From, To, Kg}.
+  Ref = uniqueref(now()),
+  ets:insert(manager, {Ref, waiting, From, To, Kg}),
+  {ok, Ref}.
 
-deliver(Loc) -> {ok, []}.
+deliver(Loc) ->
+  Pickups = ets:select(manager, [{{'$1', '$2', '$3', '$4', '$5'}, [{'==', '$3', Loc}], ['$$']}]),
+  {ok, Pickups}.
 
 loop() ->
   io:format("In loop"),
   receive
-    {_From, _To, _Kg} ->
-      io:format("Message receieved ~p~n", [now()]),
-      {ok, now()},
+    {From, To, Kg} ->
+      io:format("Message receieved ~p~n", [self()]),
+      send(From, To, Kg),
       loop();
     exit ->
       io:format("Exiting"),
