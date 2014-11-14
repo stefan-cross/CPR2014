5940fc0117dd4e160f79ab15fad1c1feacc0d0d3
stefancross <stefan.cross@o2.com>
2014-11-14 13:25:10 +0000

    Tightening logic around deliver and reserve functions

--- src/manager.erl
+++ src/manager.erl
@@ -10,10 +10,11 @@
 -author("stefancross").
 
 %% API
--export([start_link/0, deliver/1, reserve/3, loop/0, send/3]).
+-export([start_link/0, deliver/1, reserve/3, reserve/2, loop/0, send/3]).
 
 start_link() ->
   register(?MODULE, spawn_link(?MODULE, loop, [])),
+  {ok, ?MODULE},
   ets:new(manager, [set, named_table]).
 
 
@@ -27,33 +28,84 @@ send(From, To, Kg) ->
   ets:insert(manager, {Ref, waiting, From, To, Kg}),
   {ok, Ref}.
 
-% doesnt concern weight restrictions
+% Assuming deliveries are potentially parcels in transit,
 deliver(Loc) ->
   %TODO deliveries that are going to Loc
+  Deliveries = ets:select(manager, [{{'$1', '$2', '$3', '$4', '$5'}, [{'==', '$2', intransit}, {'==', '$4', Loc}], [['$1', '$2', '$3']]}]),
   Pickups = ets:select(manager, [{{'$1', '$2', '$3', '$4', '$5'}, [{'==', '$2', waiting}, {'==', '$3', Loc}], [['$1', '$2', '$3']]}]),
-  Length = length(Pickups),
+  Order = Pickups++Deliveries,
+  Length = length(Order),
   if
-    Length > 0 -> {ok, Pickups};
+    Length > 0 -> {ok, Order};
     Length =< 0 -> {error, instance}
   end.
 
+%% 51> manager:send("A", "B", 10).
+%% {ok,1415971287284657}
+%% 52> manager:send("A", "C", 10).
+%% {ok,1415971289820290}
+%% 53> manager:send("A", "D", 10).
+%% {ok,1415971292269273}
+%% 54> manager:send("A", "B", 10).
+%% {ok,1415971293837010}
+%% 55> manager:send("A", "B", 20).
+%% {ok,1415971296020752}
+%% 56> ets:insert(manager, {1415971287284612,intransit,"A","B",10}).
+%% true
+%% 58> ets:insert(manager, {1415971287284000,intransit,"B","A",10}).
+%% true
+%% 59> manager:deliver("A").
+%% {ok,[[1415971292269273,waiting,"A"],
+%% [1415971293837010,waiting,"A"],
+%% [1415971287284657,waiting,"A"],
+%% [1415971296020752,waiting,"A"],
+%% [1415971289820290,waiting,"A"],
+%% [1415971287284000,intransit,"B"]]}
+%% 60>
+
+
+
+
+%TODO consider allocation to vehicle
 reserve(From, To, Kg) ->
-  Reserved = ets:select(manager, [{{'$1', '$2', '$3', '$4', '$5'}, [{'==', '$2', waiting}, {'==', '$3', From}, {'==', '$4', To}], ['$$']}]),
+  % Reverse select not quite ordered, but improvement on ets:select which gets oldest first
+  Reserved = ets:select_reverse(manager, [{{'$1', '$2', '$3', '$4', '$5'}, [{'==', '$2', waiting}, {'==', '$3', From}, {'==', '$4', To}], ['$$']}]),
   WeightedReserve = weightedReserve(Reserved, Kg),
-  %TODO update status to reserved of selected upto weight limit, rather then delete them
-  removeReserved(WeightedReserve),
+  updateReserved(WeightedReserve),
   {ok, WeightedReserve}.
+
+%TODO consider allocation to vehicle
+reserve(From, Kg) ->
+  % Reverse select not quite ordered, but improvement on ets:select which gets oldest first
+  Reserved = ets:select_reverse(manager, [{{'$1', '$2', '$3', '$4', '$5'}, [{'==', '$2', waiting}, {'==', '$3', From}], ['$$']}]),
+  WeightedReserve = weightedReserve(Reserved, Kg),
+  updateReserved(WeightedReserve),
+  {ok, WeightedReserve}.
+
 weightedReserve([[Ref, _Status, From, To, Kg] | T], Ac) when Ac >= Kg ->
   [[Ref, reserved, From, To, Kg] | weightedReserve(T, (Ac - Kg))];
 weightedReserve([[_Ref, _Status, _From, _To, _Kg] | _T], _Ac) -> [];
 weightedReserve([], _Ac) -> [].
-removeReserved([[Ref, _Status, From, To, Kg] | T]) ->
+updateReserved([[Ref, _Status, From, To, Kg] | T]) ->
   ets:delete(manager, Ref),
   % unfortuantely we cant update bags, only sets,
   ets:insert(manager, {Ref, reserved, From, To, Kg}),
-  removeReserved(T);
-removeReserved([]) -> ok.
-
+  updateReserved(T);
+updateReserved([]) -> ok.
+%% working!
+%% 92> c(manager).
+%% {ok,manager}
+%% 93> manager:start_link().
+%% In loopmanager
+%% 94> manager:send("A", "B", 10).
+%% {ok,1415965996288405}
+%% 95> manager:send("A", "B", 10).
+%% {ok,1415965996952045}
+%% 96> manager:send("A", "B", 10).
+%% {ok,1415965997607945}
+%% 97> manager:reserve("A", "B", 20).
+%% {ok,[[1415965997607945,reserved,"A","B",10],
+%% [1415965996288405,reserved,"A","B",10]]}
 
 loop() ->
   io:format("In loop"),
